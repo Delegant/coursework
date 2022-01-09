f981e47115418efa9db55743fab38d7fdad7cf04
Anton <Antosah1@mail.ru>
2022-01-09 21:09:48 +0300

    done rev01

---
+++ src/Easy.java
@@ -0,0 +1,72 @@
+public class Easy {
+
+    public static Employee[] employees = new Employee[10];
+
+    public static void main(String[] args) {
+        employees[0] = new Employee("Ivanov Ivan Ivanovich",1,200 );
+        employees[1] = new Employee("Sidorov Michail",2,300 );
+        employees[2] = new Employee("Agent Smith",3,1000 );
+        employees[3] = new Employee("Mr Andersen",1,1 );
+        printEmployees();
+        System.out.println("Затраты на зарплату: " + costsSum());
+        System.out.println("Минимальная зарплата: " + findEmployeeMinSalary());
+        System.out.println("Максимальная зарплата: " + findEmployeeMaxSalary());
+        System.out.println("Средняя зарплата: " + meanSalary());
+        printFullNameEmployees();
+    }
+
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    public static float costsSum() {
+        float costsSum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                costsSum += employee.getSalary();
+            }
+        }
+        return costsSum;
+    }
+
+    public static Employee findEmployeeMinSalary() {
+        float lesser = Float.MAX_VALUE;
+        Employee result = null;
+        for (Employee employee : employees) {
+            if (employee != null && lesser > employee.getSalary()) {
+                lesser = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    public static Employee findEmployeeMaxSalary() {
+        float largest = Float.MIN_VALUE;
+        Employee result = null;
+        for (Employee employee : employees) {
+            if (employee != null && largest < employee.getSalary()) {
+                largest = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    public static float meanSalary() {
+        return costsSum() / Employee.getIdCounter();
+    }
+
+    public static void printFullNameEmployees() {
+        for (Employee employee:employees){
+            if (employee!=null){
+                System.out.println(employee.getFullName());
+            }
+        }
+    }
+
+}
--- src/Employee.java
+++ src/Employee.java
@@ -1,22 +1,15 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
-
 public class Employee {
     private final String fullName;
     private int department;
     private float salary;
-    private static int size;
+    private static int idCounter;
     private final int id;
 
     public Employee(String fullName, int department, float salary) {
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
-        this.id = size++;
+        this.id = idCounter++;
     }
 
     public void setDepartment(int department) {
@@ -43,156 +36,16 @@ public class Employee {
         return salary;
     }
 
-    public static int getSize() {
-        return size;
-    }
-
-    public static CharSequence listEmployees(Employee[] employees) {
-        return listEmployees(employees,0);
-    }
-
-    public static CharSequence listEmployees(Employee[] employees, int department) {
-        StringBuilder listEmployeesBuilder = new StringBuilder();
-        boolean needDepartment = true;
-        if (department !=0) {
-            employees = filterByDepartment(employees, department);
-            needDepartment = false;
-        }
-        for (Employee employee : employees) {
-            if (employee != null) {
-                listEmployeesBuilder.append("\n").append(employee.toString(true,true,needDepartment,true));
-            }
-        }
-        return listEmployeesBuilder;
-    }
-
-    public static float costsSum(Employee[] employees) {
-        float costsSum = 0;
-        for (Employee employee : employees) {
-            if (employee != null) {
-                costsSum += employee.getSalary();
-            }
-        }
-        return costsSum;
-    }
-
-    public static float costsSum(Employee[] employees, int department) {
-        employees = filterByDepartment(employees, department);
-        return costsSum(employees);
-    }
-
-    public static Employee findEmployeeMinSalary(Employee[] employees) {
-        Employee largest = employees[0];
-        for (Employee employee : employees) {
-            if (employee != null) {
-                if (largest.getSalary() > employee.getSalary()) largest = employee;
-            }
-        }
-        return largest;
-    }
-
-    public static Employee findEmployeeMinSalary(Employee[] employees, int department) {
-        return findEmployeeMinSalary(filterByDepartment(employees, department));
-    }
-
-    public static Employee findEmployeeMaxSalary(Employee[] employees) {
-        Employee largest = employees[0];
-        for (Employee employee : employees) {
-            if (employee != null) {
-                if (largest.getSalary() < employee.getSalary()) largest = employee;
-            }
-        }
-        return largest;
-    }
-
-    public static Employee findEmployeeMaxSalary(Employee[] employees, int department) {
-        return findEmployeeMaxSalary(filterByDepartment(employees, department));
-    }
-
-    public static float meanSalary(Employee[] employees) {
-        return costsSum(employees) / size;
-    }
-
-    public static float meanSalary(Employee[] employees, int department) {
-        Employee[] filteredEmployees = filterByDepartment(employees, department);
-        return costsSum(filteredEmployees) / filteredEmployees.length;
-    }
-
-    public static CharSequence listFullNameEmployees(Employee[] employees) {
-        StringBuilder allFullNameBuilder = new StringBuilder();
-        for (Employee employee : employees) {
-            if (employee != null) {
-                allFullNameBuilder.append("\n").append(employee.toString(true,false,false,false));
-            }
-        }
-        return allFullNameBuilder;
-    }
-
-    public static CharSequence listFullNameEmployees(Employee[] employees, int department) {
-        return listFullNameEmployees(filterByDepartment(employees, department));
-    }
-
-    public static int indexingSalary(Employee[] employees, int percent) {
-        for (Employee employee : employees) {
-            if (employee != null) {
-                employee.setSalary(employee.getSalary() + employee.getSalary() / 100 * percent);
-            }
-        }
-        return percent;
-    }
-
-    public static CharSequence employeeWithSalaryMore(Employee[] employees, int salary){
-        StringBuilder salaryMoreBuilder = new StringBuilder();
-        salaryMoreBuilder.append(salary).append(":\n");
-        for (Employee employee : employees) {
-            if (employee != null) {
-                if (employee.getSalary() >= salary) {
-                    salaryMoreBuilder.append("\n").append(employee.toString(true,true,true,true));
-                }
-            }
-        }
-        return salaryMoreBuilder;
-    }
-
-    public static CharSequence employeeWithSalaryLess(Employee[] employees, int salary){
-        StringBuilder salaryLessBuilder = new StringBuilder();
-        salaryLessBuilder.append(salary).append(":\n");
-        for (Employee employee : employees) {
-            if (employee != null) {
-                if (employee.getSalary() < salary) {
-                    salaryLessBuilder.append('\n').append(employee.toString(true,true,true,true));
-                }
-            }
-        }
-        return salaryLessBuilder;
-    }
-
-    public static int indexingSalary(Employee[] employees, int percent, int department) {
-        return indexingSalary(filterByDepartment(employees, department), percent);
-    }
-
-    public static Employee[] filterByDepartment(Employee[] employees, int department) {
-        Employee[] departmentEmployees = employees.clone();
-        for (int i = 0; i<employees.length; ++i) {
-            if (employees[i] != null) {
-                if (employees[i].getDepartment() != department) departmentEmployees[i] = null;
-            }
-        }
-        return departmentEmployees;
+    public static int getIdCounter() {
+        return idCounter;
     }
 
     @Override
     public String toString() {
-        return this.toString(true,true,true,true);
-    }
-
-    public String toString(boolean needFullName, boolean needSalary, boolean needDepartment, boolean needID) {
-        StringBuilder resultString = new StringBuilder();
-        if (needFullName)  resultString.append("Full Name: ").append(fullName).append(" ");
-        if (needSalary) resultString.append("Salary: ").append(salary).append(" ");
-        if (needDepartment) resultString.append("Department: ").append(department).append(" ");
-        if (needID) resultString.append("ID: ").append(id).append(" ");
-        return resultString.toString();
+        return "Full Name: " + fullName +
+                ", Salary: " + salary +
+                ", Department: " + department +
+                ", ID: " + id;
     }
 
 }
